a30cb6a3e358ffdccd011756f197556e91b47e57
Nohbin2 <98073212+Nohbin2@users.noreply.github.com>
2022-01-20 14:08:58 +0900

    if문 2753문제

--- 알고리즘/year.cpp
+++ 알고리즘/year.cpp
@@ -3,8 +3,8 @@
 int main(void) {
   int year;
   scanf("%d", &year);
-
-  if(year % 4 && year % 400 || !(year % 100 == 0))
+ 
+  if((year % 4 == 0 && year % 100 != 0) || year % 400 ==0)
   {
     printf("1\n");
   }
